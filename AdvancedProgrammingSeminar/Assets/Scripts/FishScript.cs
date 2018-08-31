4867a1c81fb887e6bfb85a7d5089a7e2239c6536
austin.laimos <42756128+AustinGitGuy@users.noreply.github.com>
2018-08-31 18:09:35 +0000

    First unity commit. Initial Project

---
+++ AdvancedProgrammingSeminar/Assets/Scripts/FishScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishScript : MonoBehaviour {
+
+    //Rigidbody2D rb;
+    float xMove, yMove;
+    float moveMod = .15f;
+
+	void Start(){
+        //rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+        GetInput();
+        Move();
+	}
+
+    void GetInput(){
+        xMove = Input.GetAxis("Horizontal");
+        yMove = Input.GetAxis("Vertical");
+    }
+
+    void Move(){
+        transform.position = new Vector2(transform.position.x + xMove * moveMod, transform.position.y + yMove * moveMod);
+    }
+}
