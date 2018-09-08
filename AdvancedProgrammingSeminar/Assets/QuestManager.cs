412e8384bbdf6a1d4bedd15d8345dd79ae41dc29
austin.laimos <42756128+AustinGitGuy@users.noreply.github.com>
2018-09-08 22:49:19 +0000

    Added a new mission.

---
+++ AdvancedProgrammingSeminar/Assets/QuestManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers {
+	public class QuestManager : Singleton<QuestManager> {
+
+		public List<bool> questsAccepted;
+		public List<bool> questsCompleted;
+		public GameObject check;
+		public GameObject x;
+		public bool checkPress;
+		public bool xPress;
+
+		void Start(){
+			Button btn1 = check.GetComponent<Button>();
+			Button btn2 = x.GetComponent<Button>();
+			btn1.onClick.AddListener(delegate {TaskWithParameters("Check"); });
+			btn2.onClick.AddListener(delegate {TaskWithParameters("X"); });
+			check.gameObject.SetActive(false);
+			x.gameObject.SetActive(false);
+		}
+
+		void TaskWithParameters(string type){
+			if(type == "Check"){
+				checkPress = true;
+			}
+			if(type == "X"){
+				xPress = true;
+			}
+		}
+
+	}
+}
