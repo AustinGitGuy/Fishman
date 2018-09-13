80c1db8cbd601845a5b13c01accb2d464cb4d841
austin.laimos <42756128+AustinGitGuy@users.noreply.github.com>
2018-09-13 18:23:25 +0000

    Added some new props in preparation for the city area.

---
+++ AdvancedProgrammingSeminar/Assets/Scripts/RespawnManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Managers {
+	public class RespawnManager : Singleton<RespawnManager> {
+		public Vector3 spawnPoint;
+		GameObject player;
+		public List<bool> questsAccepted;
+		public List<bool> questsCompleted;
+		public GameObject[] targets;
+		public GameObject[] guards;
+
+		void Start(){
+			player = Managers.PlayerManager.Instance.GetPlayer();
+			targets = GameObject.FindGameObjectsWithTag("Target");
+			guards = GameObject.FindGameObjectsWithTag("Guard");
+		}
+		public void Respawn(){
+			Managers.PlayerManager.Instance.health = 10f;
+			player.transform.position = spawnPoint;
+			for(int i = 0; i < questsCompleted.Count; i++){
+				for(int c = 0; c < targets.Length; i++){
+					if(targets[c].GetComponent<TargetScript>().id == i){
+						if(!questsCompleted[i]){
+							targets[c].GetComponent<GuardScript>().dead = false;
+							targets[c].transform.position = targets[c].GetComponent<GuardScript>().spawnPos;
+						}
+					}
+				}
+			}
+			for(int i = 0; i < guards.Length; i++){
+				guards[i].GetComponent<GuardScript>().dead = false;
+				guards[i].transform.position = guards[i].GetComponent<GuardScript>().spawnPos;
+			}
+		}
+	}
+}
