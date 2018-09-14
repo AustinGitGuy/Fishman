d8d93ea6c1b9a428fdb3ac4281c7d888df844da9
austin.laimos <42756128+AustinGitGuy@users.noreply.github.com>
2018-09-14 00:17:45 +0000

    Added most of the laboratory, just need to add doors and some more researchers.

--- AdvancedProgrammingSeminar/Assets/Scripts/RespawnManager.cs
+++ AdvancedProgrammingSeminar/Assets/Scripts/RespawnManager.cs
@@ -18,9 +18,10 @@ namespace Managers {
 		}
 		public void Respawn(){
 			Managers.PlayerManager.Instance.health = 10f;
+			Managers.NPCManager.Instance.huntPlayer = false;
 			player.transform.position = spawnPoint;
 			for(int i = 0; i < questsCompleted.Count; i++){
-				for(int c = 0; c < targets.Length; i++){
+				for(int c = 0; c < targets.Length; c++){
 					if(targets[c].GetComponent<TargetScript>().id == i){
 						if(!questsCompleted[i]){
 							targets[c].GetComponent<GuardScript>().dead = false;
