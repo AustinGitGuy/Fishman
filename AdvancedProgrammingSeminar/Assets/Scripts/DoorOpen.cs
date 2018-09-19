a229aa064ed51ee3c111a0a619c967f0b91da219
austin.laimos <austin.laimos@8a647ddd-2976-492a-97e1-44e6ac29c8de>
2018-09-19 04:55:05 +0000

    Did some more work on lab, just need to add the card holders in now

---
+++ AdvancedProgrammingSeminar/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+	GameObject player;
+	public GameObject door;
+	public bool vertical;
+	public string color;
+
+	void Start(){
+		
+	}
+	
+	void Update(){
+		
+	}
+}
