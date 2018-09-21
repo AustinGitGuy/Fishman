1e82bb2993cff5609c695dbe142ed5ac74f41252
austin.laimos <austin.laimos@8a647ddd-2976-492a-97e1-44e6ac29c8de>
2018-09-21 00:23:08 +0000

    Fixed some bugs related to player firing and guard detection.

--- AdvancedProgrammingSeminar/Assets/Scripts/LaunchBullet.cs
+++ AdvancedProgrammingSeminar/Assets/Scripts/LaunchBullet.cs
@@ -13,12 +13,18 @@ public class LaunchBullet : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D>();
 		rb.AddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * forceAmount);
 		Destroy(this.gameObject, 10f);
-		player.GetComponent<FishScript>().crimeLevel -= 20f;
+        StartCoroutine(FallBack());
 	}
+
+    IEnumerator FallBack(){
+        yield return new WaitForSeconds(1f);
+        player.GetComponent<FishScript>().crimeLevel -= 20f;
+    }
 
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.tag == "Target" || col.gameObject.tag == "Client" || col.gameObject.tag == "Guard" || col.gameObject.tag == "Civilian"){
-			Destroy(this.gameObject);
+            player.GetComponent<FishScript>().crimeLevel -= 20f;
+            Destroy(this.gameObject);
 		} 
 		else {
 			Destroy(this.gameObject, 2f);
