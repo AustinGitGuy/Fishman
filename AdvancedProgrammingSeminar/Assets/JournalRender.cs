4929226eab159dfbf2ae94ee4098e16c59a04eaa
austin.laimos <42756128+AustinGitGuy@users.noreply.github.com>
2018-10-18 18:38:55 +0000

    Did some more work in journals.

---
+++ AdvancedProgrammingSeminar/Assets/JournalRender.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalRender : MonoBehaviour {
+
+	public ViewQuest[] quests;
+	[SerializeField]
+	GiveMission[] clients;
+	GameObject journal;
+	bool journalActive;
+
+	void Start(){
+		for(int i = 0; i < clients.Length; i++){
+			clients[i] = GameObject.Find("Client" + i).GetComponent<GiveMission>();
+		}
+		journal = GameObject.Find("Journal");
+		journal.SetActive(false);
+	}
+	
+	void Update(){
+		CheckInput();
+	}
+
+	public void GetEmptySlot(int id){
+		for(int i = 0; i < quests.Length; i++){
+			if(quests[i].id == id) return;
+			if(quests[i].id == -1){
+				quests[i].id = id;
+				quests[i].targetName = clients[id].target.name;
+				return;
+			}
+		}
+	}
+
+	void CheckInput(){
+		if(Input.GetKeyDown(KeyCode.J)){
+			journalActive = !journalActive;
+			journal.SetActive(journalActive);
+		}
+	}
+}
---
+++ AdvancedProgrammingSeminar/Assets/ViewQuest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewQuest : MonoBehaviour {
+
+	public int id;
+	public string targetName;
+	Text questText;
+	Text viewText;
+
+	void Start(){
+		questText = GetComponent<Text>();
+		viewText = transform.Find("View").GetComponent<Text>();
+		id = -1;
+	}
+	
+	void Update(){
+		if(id == -1){
+			questText.text = "";
+			viewText.text = "";
+		}
+		else {
+			viewText.text = "View Here";
+			questText.text = targetName;
+		}
+	}
+}
