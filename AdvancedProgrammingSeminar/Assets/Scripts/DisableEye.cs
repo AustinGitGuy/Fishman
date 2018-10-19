37a22d66880802be22a30cced8c06ef5d28aa270
austin.laimos <42756128+AustinGitGuy@users.noreply.github.com>
2018-10-19 00:17:32 +0000

    Added some more feedback (Particle Systems and stuff), finished journal and added inspect/reminder text to certain objects.

--- AdvancedProgrammingSeminar/Assets/Scripts/DisableEye.cs
+++ AdvancedProgrammingSeminar/Assets/Scripts/DisableEye.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class DisableEye : MonoBehaviour {
 
 	GameObject eye;
+	ParticleSystem sys;
+	bool deactivate;
 
 	void Start(){
 		eye = transform.Find("Eye").gameObject;
 		eye.SetActive(false);
+		sys = transform.Find("ParticleSystem").GetComponent<ParticleSystem>();
 	}
 	
 	void Update(){
 		if(Managers.NPCManager.Instance.seePlayer){
 			eye.SetActive(true);
+			if(deactivate){
+				sys.Play();
+				deactivate = false;
+			}
 		}
 		else {
 			eye.SetActive(false);
+			deactivate = true;
 		}
 	}
 }
